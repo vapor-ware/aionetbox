02bc2408f10f255fbca8e692c11d69253abf7de0
Charles Butler <chuck@vapor.io>
2019-09-09 16:32:35 -0500

    Lint cleanup
    - refactoring code style and nits for pylint

--- aio_netbox/client.py
+++ aio_netbox/client.py
@@ -1,10 +1,22 @@
-import os
+"""
+here's a docstring
+"""
+
 import asyncio
 import aiohttp
-import json
 
 # AIOHTTP request helper
-class AIONetbox(object):
+class AIONetbox():
+    """AIONetbox
+
+    An AIOHTTP client implementation for Netbox. Specifically with an eye
+    towards non-blocking client requests.
+
+    Todo:
+            * retry-logic
+            * exception catching
+    """
+
     def __init__(self, *, host=None, port=None, auth_token=None, loop=None):
         self.host = host
         self.port = port
@@ -13,90 +25,59 @@ class AIONetbox(object):
         self._client = None
 
     def client(self):
+        """
+        An AIO HTTP request client factory for interfacing with netbox
+        """
         if not self._client:
             self._client = aiohttp.ClientSession()
 
         return self._client
 
+
     def _build_url(self, route, **kwargs):
-        return '{}{}/api{}'.format(self.host, ':{}'.format(self.port) if self.port else '', route)
+        """
+          _build_url - concatenate details to build the netbox path to query
+        """
+        # Route will be kind to the user and prepend the / if omitted
+        if not route.startswith('/'):
+            route = '/{}'.format(route)
+
+        # build the base url
+        url = '{}{}/api{}'.format(self.host,
+                                  ':{}'.format(self.port) if self.port
+                                  else '', route)
+
+        # convert key=value pairs into URL parameters
+        if kwargs:
+            url = '{}?{}'.format(url,
+                                 '&'.join('{}={}'.format(key, val) \
+                                          for key, val in kwargs.items()))
+        return url
+
 
     async def get(self, route, **kwargs):
-        # TODO: Make this invoke a __fetdch_all() method that knows how to
-        # speak the pagination of netbox so we're dealing with flat frames
-        # of data
-        headers = { 'Authorization': "Token {}".format(self.auth_token),
-                    'content-type': "application/json"}
+        """
+        get - the outer method to invoke the actual http request
 
-        # Kind of hacky but append any args as parameters to the query
-        if kwargs:
-          url = "{}?{}".format(self._build_url(route), '&'.join('{}={}'.format(key, val) for key, val in kwargs.items()))
+        TODO: Make this invoke a __fetch_all() method that knows how to
+        speak the pagination of netbox so we're dealing with flat frames
+        of data
+        """
+        headers = {'Authorization': 'Token {}'.format(self.auth_token),
+                   'content-type': 'text/plain'}
+
+        url = self._build_url(route, **kwargs)
 
         async with self.client().get(url, headers=headers) as resp:
-            return (await resp.json())['results']
+            body = await resp.json(content_type='application/json')
+            if 'results' in body.keys():
+                return body['results']
+            else:
+                raise ValueError("Did not receive the data we expected")
+
 
     async def close(self):
+        """
+        close the socket connection
+        """
         await self.client().close()
-
-
-# Main method
-async def run(token, tenant_id=None):
-    nb = AIONetbox(host='https://netbox.preview.cloud.vio.sh', auth_token=token)
-    results = {}
-    lockers = await nb.get('/dcim/devices', role="customer-network-locker")
-
-    # filter for just the tenants lockers
-    tenant_lockers = [x for x in lockers if (x["tenant"] != None) and (x["tenant"]["id"] == tenant_id) ]
-    # With our list of lockers, we can iterate the connections to extract the network
-    # devices (ports)
-    short_list = [x["name"] for x in tenant_lockers]
-
-    tenant_ports = await nb.get('/dcim/interface-connections', devices=short_list)
-
-    for locker in lockers:
-        # search through interface b, which is the customer locker interface by convention in our netbox data
-        device_interfaces = [ y for y in tenant_ports if (y["interface_b"] != None) and (y["interface_b"]["device"]["id"] == locker["id"]) ]
-
-        # Get all the network switch details
-        switches = [ p["interface_a"] for p in device_interfaces if p["interface_a"] !=  None ]
-        # List placeholder for the modified objects
-        switches_dense = []
-        tenant_ifaces = []
-        # Since we query by switch id, there should only ever be 1... at least I hope.
-        for switch in switches:
-            iface_detail = await nb.get('/dcim/interfaces', id=switch["id"])
-            # fetch the device LAG and TYPE for switches for the UI:
-            switch["type"] = iface_detail[0]["type"]
-            switch["lag"] = iface_detail[0]["lag"]
-            switches_dense.append(switch)
-
-        # the same logic for the client connections
-        client_ifaces = [ p["interface_b"] for p in device_interfaces if p["interface_b"] != None ]
-        for iface in client_ifaces:
-            iface_detail = await nb.get('/dcim/interfaces', id=iface["id"])
-            # fetch the device LAG and TYPE for Client Ifaces for the UI:
-            iface["type"] = iface_detail[0]["type"]
-            iface["lag"] = iface_detail[0]["lag"]
-            tenant_ifaces.append(iface)
-
-        # finally update our results object
-        results.update({ locker["name"]: {
-                     "id": locker["id"],
-                     "display_name": locker["display_name"],
-                     "device_type": locker["device_type"],
-                     "interfaces":
-                        tenant_ifaces,
-                     "switches":
-                        switches_dense,
-                   } })
-
-
-
-
-    await nb.close()
-    print(json.dumps(results, indent=3))
-
-if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(run(os.environ.get('NETBOX_TOKEN'), os.environ.get('TENANT_ID')))
-
--- tests/unit/test_aionetbox_class.py
+++ tests/unit/test_aionetbox_class.py
@@ -1,11 +1,53 @@
+"""
+module docstring
+"""
 import pytest
+import asynctest
+from mock import MagicMock
+from mock import patch
 
 from aio_netbox.client import AIONetbox
 
-class TestAIONetboxClient(object):
+
+class TestAIONetboxClient():
     """ Netbox AIOHTTP Client tests """
 
-    def test_query_lockers(self):
-        """ Test retrieval of lockers """
-        tclient = AIONetbox(host="localhost", auth_token="TEST1234")
+    def test_url_builder(self):
+        """ Test that we get what we expect from our inputs when building the
+        URL"""
+
+        tclient = AIONetbox(host='localhost', auth_token='mytoken')
+        assert tclient._build_url('/dcim/foo') == 'localhost/api/dcim/foo'
+
+
+    def test_url_builder_with_bunk_route(self):
+        """ test with a missing forward slash in the route """
+
+        tclient = AIONetbox(host='localhost', auth_token='mytoken')
+        assert tclient._build_url('dcim/foo') == 'localhost/api/dcim/foo'
+
+
+    def test_url_builder_with_kwarg_valid_params(self):
+        """ test with key/value pairs that should be serialized into url params
+        """
+
+        tclient = AIONetbox(host='localhost', auth_token='mytoken')
+        assert tclient._build_url('dcim/foo', foo='bar') == 'localhost/api/dcim/foo?foo=bar'
+
+
+    def test_url_builder_with_kwarg_invalid_params(self):
+        """ test with key/value pairs that should be serialized into url params
+        """
+
+        tclient = AIONetbox(host='localhost', auth_token='mytoken')
+        assert tclient._build_url('dcim/foo', invalid='') == 'localhost/api/dcim/foo?invalid='
+
+    @pytest.mark.asyncio
+    async def test_get_passes_auth_token(self):
+        """ stuff and things """
+        with patch.object(AIONetbox, 'client', return_value = asynctest.MagicMock()) as aiobutts:
+            tclient = AIONetbox(host='localhost', auth_token='mytoken')
+            print(tclient.client)
+            resp = await tclient.get('dcim/foo')
+            print(resp)
 
