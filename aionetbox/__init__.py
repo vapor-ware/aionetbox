10e204fb01404365755e82a07907578d461d684c
Marco Ceppi <marco@ceppi.net>
2020-09-14 15:02:31 -0400

    :bookmark: 1.7.1

--- aionetbox/__init__.py
+++ aionetbox/__init__.py
@@ -2,7 +2,7 @@
 from .api import AIONetbox
 
 __title__ = 'aionetbox'
-__version__ = '1.7.0'
+__version__ = '1.7.1'
 __description__ = 'Async netbox library'
 __author__ = 'Marco Ceppi'
 __author_email__ = 'marco@vapor.io'
